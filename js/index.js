1f4aa380bb7fe4da5c6ff72c4d63fdbce7190e28
henybeza <henny8000@gmail.com>
2020-07-28 00:57:13 -0700

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let myLogo = document.querySelector('#myLogo');
+myLogo.addEventListener('click', changeLogo);
+
+function changeLogo(){
+this.innerHTML = "<marquee><h3>Selam Wesenaiiiiii</h3></marquee>";
+};
+
+
+let myHeader = document.querySelector('.myHeader');
+myHeaders.addEventListener('click', changeHeaderFont);
+ function changeHeaderFont(){
+     this.innerHTML = "<h2>Welcome to our parish...</h2>";
+ }
